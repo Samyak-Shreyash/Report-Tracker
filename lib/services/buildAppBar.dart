0c6d57352d2e36af99b55c9903b3d48632df20d2
Samyak-Shreyash <sam.shreyash@gmail.com>
2020-08-14 18:18:36 +0530

    Added Support for Dark Theme

---
+++ lib/services/buildAppBar.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:report_tracker/services/theme.dart';
+
+class BuildAppBar
+{
+  bool _themeDark =true;
+  ThemeData _themeSet = ThemeData.light();
+  IconData _themeIcon = Icons.wb_sunny;
+
+  AppBar buildBar(ThemeChanger _themeChanger, String title) {
+    return AppBar(
+      title: Text(title),
+      actions: [
+        IconButton(
+            icon: !_themeDark?Icon(Icons.wb_sunny):Icon(Icons.cloud_queue),
+            onPressed: () =>
+            themeUpdate(_themeChanger))
+      ],
+    );
+  }
+
+  void themeUpdate(ThemeChanger _themeChanger) {
+    _themeDark = !_themeDark;
+    if (_themeDark)
+    {
+      IconData _themeIcon = Icons.wb_sunny;
+      _themeChanger.setTheme(ThemeData.light());
+    }
+    else
+    {
+      IconData _themeIcon = Icons.cloud_queue;
+      _themeChanger.setTheme(ThemeData.dark());
+    }
+    print(_themeDark);
+
+  }
+}
